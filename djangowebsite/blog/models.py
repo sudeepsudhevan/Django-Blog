38543623cc0a08452c9a8801847be146a9fc34b4
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-12-30 10:14:37 +0530

    comments tags

--- djangowebsite/blog/models.py
+++ djangowebsite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -29,6 +30,7 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ("-publish",)
--- djangowebsite/blog/views.py
+++ djangowebsite/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .forms import EmailPostForm
-from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
+
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
@@ -24,8 +25,27 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
 
-    return render(request, "blog/post/detail.html", {"post": post})
+    return render(
+        request,
+        "blog/post/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "new_comment": new_comment,
+            "comment_form": comment_form,
+        },
+    )
 
 
 def post_share(request, post_id):
