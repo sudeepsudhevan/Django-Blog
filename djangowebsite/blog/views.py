c7f27440c0059e7fabaee1a255cf60f3341be50e
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-12-30 10:49:58 +0530

    tags added

--- djangowebsite/blog/views.py
+++ djangowebsite/blog/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
-
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
+
 
 # Create your views here.
-
-
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = "posts"
-    paginate_by = 3
-    template_name = "blog/post/list.html"
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(
+        request,
+        "blog/post/list.html",
+        {"page": page, "posts": posts, "tag": tag},
+    )
 
 
 def post_detail(request, year, month, day, post):
@@ -36,6 +52,12 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
+
     return render(
         request,
         "blog/post/detail.html",
@@ -44,6 +66,7 @@ def post_detail(request, year, month, day, post):
             "comments": comments,
             "new_comment": new_comment,
             "comment_form": comment_form,
+            "similar_posts": similar_posts,
         },
     )
 
